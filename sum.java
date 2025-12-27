4a5f4fe0c167eba4cbd3f63f4740379851814409
dayani gautam <dayanigautam789@gmail.com>
2025-12-27 14:25:28 +0530

    add implement

--- sum.java
+++ sum.java
@@ -2,8 +2,4 @@ class Addition {
     public static void main (String [] args){
   int a = 10;
   int b = 30;
-  int sum = a+b;
-  System.out.println("Sum =" + sum);
-  }
-}
-//My Name is Gautam.
+  
